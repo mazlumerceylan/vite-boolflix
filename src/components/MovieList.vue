6537b707a479bb0a3633b4fdafe0cf89941e576b
mazlumerceylan <mazlumerceylan98@gmail.com>
2023-05-08 01:58:17 +0200

    milestone 2 aggiunta bandiera e serie tv

--- src/components/MovieList.vue
+++ src/components/MovieList.vue
@@ -2,8 +2,21 @@
   <div class="movie-list">
     <div v-for="movie in movies" :key="movie.id" class="movie-item">
       <h3>{{ movie.title }}</h3>
-      <p><strong>Titolo Originale:</strong> {{ movie.original_title }}</p>
-      <p><strong>Lingua:</strong> {{ movie.original_language }}</p>
+      <p><strong>Titolo Originale:</strong>     {{ movie.original_title }}</p>
+      <p>
+        <strong>Lingua:</strong>
+        <span v-if="movie.original_language === 'it'">
+          <img src="src/assets/images/flags/it.png" alt="Italian flag" width="20" height="20">
+          {{ movie.original_language }}
+        </span>
+        <span v-else-if="movie.original_language === 'en'">
+          <img src="src/assets/images/flags/en.png" alt="English flag" width="20" height="20">
+          {{ movie.original_language }}
+        </span>
+        <span v-else>
+          {{ movie.original_language }}
+        </span>
+      </p>
       <p><strong>Voto:</strong> {{ movie.vote_average }}/10</p>
       <p>{{ movie.overview }}</p>
     </div>
@@ -22,5 +35,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+
+@import 'src/scss/MovieList.scss';
 
 </style>
