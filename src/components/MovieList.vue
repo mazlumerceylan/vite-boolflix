ab44598ca31686fef83b188f1116fa5b8d491227
mazlumerceylan <mazlumerceylan98@gmail.com>
2023-05-08 02:26:17 +0200

    aggiunta stelle voti

--- src/components/MovieList.vue
+++ src/components/MovieList.vue
@@ -2,22 +2,28 @@
   <div class="movie-list">
     <div v-for="movie in movies" :key="movie.id" class="movie-item">
       <h3>{{ movie.title }}</h3>
-      <p><strong>Titolo Originale:</strong>     {{ movie.original_title }}</p>
+      <p><strong>Titolo Originale:  </strong> {{ movie.original_title }}</p>
       <p>
-        <strong>Lingua:</strong>
+        <strong>Lingua:   </strong>
         <span v-if="movie.original_language === 'it'">
           <img src="src/assets/images/flags/it.png" alt="Italian flag" width="20" height="20">
-          {{ movie.original_language }}
         </span>
         <span v-else-if="movie.original_language === 'en'">
           <img src="src/assets/images/flags/en.png" alt="English flag" width="20" height="20">
-          {{ movie.original_language }}
         </span>
         <span v-else>
           {{ movie.original_language }}
         </span>
       </p>
-      <p><strong>Voto:</strong> {{ movie.vote_average }}/10</p>
+      <p>
+        <strong>Voto:</strong>
+        <span class="rating">
+          <template v-for="i in 5">
+            <i v-if="i <= Math.ceil(movie.vote_average / 2)" class="fas fa-star"></i>
+            <i v-else class="far fa-star"></i>
+          </template>
+        </span>
+      </p> 
       <p>{{ movie.overview }}</p>
     </div>
   </div>
